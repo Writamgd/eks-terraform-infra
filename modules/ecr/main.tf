36064efe7f204f8b165a9fd32bd72d4c473f6321
Writamgd <71660758+Writamgd@users.noreply.github.com>
2026-02-28 12:43:42 +0530

    Updated the ECR lifecycle

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -9,3 +9,23 @@ resource "aws_ecr_repository" "repo" {
     Environment = "dev"
   }
 }
+resource "aws_ecr_lifecycle_policy" "repo_policy" {
+  repository = aws_ecr_repository.repo.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 20 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 20
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
